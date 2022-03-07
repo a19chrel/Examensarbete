5dee38260df8668a7d72ead9758768760b66af71
Christian Elmhäll <a19chrel@student.his.se>
2022-03-07 14:56:41 +0100

    init + dataExtractor added

---
+++ dataExtractor.js
@@ -0,0 +1,55 @@
+const fs = require('fs');
+const readline = require('readline');
+
+async function extractData() {
+    // Settings
+    const dataStartRow = 805;
+    const startIndex = 0;
+    const numOfObject = 40000000;
+    const objectPerFile = 1000000;
+
+    const fileStream = fs.createReadStream('rows.json');
+    const rl = readline.createInterface({
+        input: fileStream,
+        crlfDelay: Infinity
+    });
+
+    let startTime = new Date().getTime();
+    let currentRow = 0;
+    let iteration = 0;
+    let fileData = [];
+
+    for await (const line of rl) {
+        //Check if the row number is higher than 805. The data starts at 805 in the original json file.
+        if (currentRow > dataStartRow + startIndex) {
+            // Remove the first two character of the row.
+            let jsonArray = JSON.parse(line.substr(2));
+
+            fileData.push({
+                created_at: jsonArray[3],
+                updated_at: jsonArray[5],
+                cdc_case_earliest_dt: jsonArray[8],
+                current_status: jsonArray[12],
+                sex: jsonArray[13],
+                age_group: jsonArray[14],
+                hosp_yn: jsonArray[16],
+                icu_yn: jsonArray[17],
+                death_yn: jsonArray[18],
+                medcond: jsonArray[19]
+            })
+            iteration = iteration + 1;
+            if (fileData.length === objectPerFile) {
+                fs.writeFileSync(`output/data${iteration/objectPerFile}.json`, JSON.stringify(fileData, null, 2));
+                fileData = [];
+            }
+            console.log(`${iteration} / ${numOfObject}`)
+        }
+        currentRow = currentRow + 1;
+        if (iteration === numOfObject) {
+            console.log(`Parsing complete! Execute time: ${new Date().getTime() - startTime}ms`)
+            return 0;
+        }
+    }
+}
+
+extractData();
