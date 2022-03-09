dc0e4b11bf7e60107768de6b83c817c13feaa82d
Christian Elmhäll <a19chrel@student.his.se>
2022-03-09 19:10:24 +0100

    Added CURD operations to MongoDB

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-var cors = require('cors');
+const cors = require('cors');
 
 /** Cross-Origin Resource Sharing (CORS) */
 var corsOptions = {
--- routes/mongodb.js
+++ routes/mongodb.js
@@ -1,19 +1,45 @@
 const router = require('express').Router();
-
-router.post('/create', (req, res) => {
-    res.send("Create endpoint for MongoDB");
+const { MongoClient } = require('mongodb');
+const client = new MongoClient("mongodb://localhost", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 });
 
-router.get('/get', (req, res) => {
-    res.send("Get endpoint for MongoDB");
+let dbConnection;
+
+client.connect(function (err, db) {
+    if (err || !db) console.log(err)
+    dbConnection = db.db('examensarbetet-pilot');
+    console.log('Successfully connected to MongoDB.');
+});
+
+router.get('/', (req, res) => {
+
+    dbConnection.collection("records").find(req.body).toArray(function (err, result) {
+        if (err) console.log(err)
+        else return res.status(200).json(result);
+    });
+})
+
+router.post('/create', (req, res) => {
+
+    dbConnection.collection("records").insertOne(req.body);
+    return res.send("Record added")
 });
 
 router.put('/update', (req, res) => {
-    res.send("Update endpoint for MongoDB");
+    dbConnection.collection("records").updateOne(
+        { "_id": ObjectId(req.params.id) },
+        {
+            $set: req.body
+        },
+        {upsert: true}
+    )
 });
 
-router.delete('/create', (req, res) => {
-    res.send("Delete endpoint for MongoDB");
+router.delete('/:id', (req, res) => {
+    dbConnection.collection("records").delete({ "_id": ObjectId(req.params.id) })
+    return res.send("Document removed.");
 });
 
 module.exports = router;
