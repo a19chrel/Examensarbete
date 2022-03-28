990e03285f817dcf2325294a6f4be6256bc48728
Christian Elmhäll <a19chrel@student.his.se>
2022-03-28 09:36:43 +0200

    Fixed database connection to Couchbase and updated the endpoints

---
+++ db/couchbase.js
@@ -0,0 +1,71 @@
+const couchbase = require('couchbase');
+
+const CB_USER = "admin"
+const CB_PASS = "password"
+const CB_URL = "localhost"
+const CB_BUCKET = "records"
+
+if (!CB_USER) {
+    throw new Error(
+        'Please define the CB_USER environment variable inside dev.env'
+    )
+}
+
+if (!CB_PASS) {
+    throw new Error(
+        'Please define the CB_PASS environment variable inside dev.env'
+    )
+}
+
+if (!CB_URL) {
+    throw new Error(
+        'Please define the CB_URL environment variable inside dev.env'
+    )
+}
+
+if (!CB_BUCKET) {
+    throw new Error(
+        'Please define the CB_BUCKET environment variable inside dev.env'
+    )
+}
+
+/**
+ * Global is used here to maintain a cached connection across hot reloads
+ * in development. This prevents connections growing exponentially
+ * during API Route usage.
+ */
+let cached = global.couchbase
+
+if (!cached) {
+    cached = global.couchbase = { conn: null }
+}
+
+async function createCouchbaseCluster() {
+    if (cached.conn) {
+        return cached.conn
+    }
+
+    cached.conn = await couchbase.connect('couchbase://' + CB_URL, {
+        username: CB_USER,
+        password: CB_PASS,
+    })
+
+    return cached.conn
+}
+
+async function connectToDatabase() {
+    const cluster = await createCouchbaseCluster()
+    const bucket = cluster.bucket(CB_BUCKET);
+    const collection = bucket.defaultCollection();
+    const profileCollection = bucket.collection('profile');
+
+    let dbConnection = {
+        cluster,
+        bucket,
+        collection,
+        profileCollection,
+    }
+
+    return dbConnection;
+}
+module.exports = { connectToDatabase };
--- routes/couchbase.js
+++ routes/couchbase.js
@@ -1,19 +1,66 @@
+const couchbase = require("couchbase");
 const router = require('express').Router();
+const {connectToDatabase} = require('../db/couchbase');
 
-router.post('/create', (req, res) => {
-   res.send("Create endpoint for Couchbase");
+
+router.post('/', async (req, res) => {
+    const {cluster} = await connectToDatabase();
+
+    try {
+        await cluster.query(`INSERT INTO records (KEY, VALUE)
+                             VALUES ("${req.body._id}", ${JSON.stringify(req.body)});`)
+            .then((response) => {
+                res.send(response.rows)
+            })
+            .catch((error) => res.status(500).send({
+                "message": `Query failed: ${error.message}`
+            }))
+    } catch (e) {
+        console.error(e)
+    }
 });
 
-router.get('/get', (req, res) => {
-    res.send("Get endpoint for Couchbase");
+router.get('/:date', async (req, res) => {
+    const {cluster} = await connectToDatabase();
+
+    try {
+        await cluster.query(`SELECT r.*
+                             FROM records r
+                             WHERE r.cdc_case_earliest_dt = "${req.params.date}";`)
+            .then((response) => {
+                res.send(response.rows)
+            })
+            .catch((error) => res.status(500).send({
+                "message": `Query failed: ${error.message}`
+            }))
+    } catch (e) {
+        console.error(e)
+    }
 });
 
-router.put('/update', (req, res) => {
-    res.send("Update endpoint for Couchbase");
-});
+router.put('/:date', async (req, res) => {
+    const {cluster} = await connectToDatabase();
 
-router.delete('/create', (req, res) => {
-    res.send("Delete endpoint for Couchbase");
+    //Query builder
+    let changesString = "";
+    Object.keys(req.body).forEach(key => {
+        changesString += `${key} = "${req.body[key]}"`;
+        if (Object.keys(req.body)[Object.keys(req.body).length - 1] !== key) changesString += `, `;
+    })
+    let query = `UPDATE records._default._default set ${changesString} WHERE cdc_case_earliest_dt = "${req.params.date}";`
+    console.log(query);
+
+    try {
+        await cluster.query(query)
+            .then((response) => {
+                res.send(response.rows)
+            })
+            .catch((error) => res.status(500).send({
+                "message": `Query failed: ${error.message}`
+            }))
+    } catch (e) {
+        console.error(e)
+    }
 });
 
 module.exports = router;
